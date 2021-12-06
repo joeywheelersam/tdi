c338ffb80fdb3cbc0dca537cb591dbc13e9f8406
joeywheeler_sam <jomar.ferreira.amorim@gmail.com>
2021-12-06 16:14:21 -0300

    Arrumei os comentários.

--- exercicio1.py
+++ exercicio1.py
@@ -17,23 +17,26 @@ alvo_ativo = True
 print(alvo_ativo)
 print("########")
 
-# Atribua à variável portas_principais uma lista com os as portas 21, 22, 23, 80, 443, 3306, 8080 (todos tipo int).
+# Atribua à variável portas_principais uma lista com os as portas
+# 21, 22, 23, 80, 443, 3306, 8080 (todos tipo int).
 portas_principais = [21, 22, 23, 80, 443, 3306, 8080]
 print(portas_principais)
 print("########")
 
-# Atribua à variável size_number o tamanho da lista numbers criada no exercício anterior. Dica - use uma função para descobrir o tamanho (len())
+# Atribua à variável size_number o tamanho da lista numbers criada no exercício anterior.
+# Dica - use uma função para descobrir o tamanho (len())
 quantas_portas = (len(portas_principais))
 print(quantas_portas)
 print("########")
 
-# Use o print para imprimir a soma das variáveis n1 e n2 (não mexa na declaração das variáveis, apenas no print).
+# Use o print para imprimir a soma das variáveis n1 e n2.
+# Obs.: não mexa na declaração das variáveis, apenas no print.
 n1 = 10
 n2 = "40"
 print(n1 + int(n2))
 print("########")
 
-# Na variavel alvo, insira no dicionário as seguintes keys e values:
+# Na variavel alvo, insira no dicionário as seguintes chaves (key) e valores (value):
 # ip = 8.8.8.8 | porta = 80 | os = linux | ativo = true
 alvo = {
   'ip': '8.8.8.8',
@@ -52,7 +55,8 @@ print("########")
 print(alvo["os"])
 print("########")
 
-# Com base na dict alvo (criada acima), adicione uma nova key chamada os_version com o value string de "1.0" (usando apenas 1 linha)..
+# Com base na dict alvo (criada acima), adicione uma nova key chamada os_version
+# com o valor da string de "1.0" (usando apenas 1 linha).
 alvo["os_version"] = "1.0"
 print(alvo)
 print("########")
@@ -66,7 +70,9 @@ alvos = ["8.8.8.8", "1.1.1.1", "208.84.244.116", "200.147.3.15", "186.192.90.12"
 print(alvos)
 print("########")
 
-# Adicione na na última posição da lista o alvo "66.96.146.129". OBS - Não altere a declaração da lista! Adicione o alvo na lista alvos com uma linha de código nova, utilizando uma função.
+# Adicione na última posição da lista o alvo "66.96.146.129".
+# Adicione o alvo na lista alvos com uma linha de código nova, utilizando uma função.
+# OBS - Não altere a declaração da lista! 
 alvos.append("66.96.146.129")
 print(alvos)
 print("########")
