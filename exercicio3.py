3cf95f1912aaa73173b654468ff924d90378d3a5
joeywheeler_sam <jomar.ferreira.amorim@gmail.com>
2021-12-06 10:32:05 -0300

    Fiz o exercício 3 e arrumei o menu do main

--- exercicio3.py
+++ exercicio3.py
@@ -1,9 +1,51 @@
 import subprocess
 import os
+import requests
+  # URL da Requests Get = r = requests.get ('https://pypi.org/project/requests/')
+  # Captura o código fonte = print (r.text) 
+  # Captura o Status Code = print (r.status_code)
 os.system('cls' if os.name == 'nt' else 'clear')
 
-print ("Exercicio 03")
+url = "uol.com.br/"
+lista = ["admin", "login", "css", "sports", "imagem", "mail", "painel"]
 
+# Menu
+print ("0 - Voltar ao Main")
+print ("1 - Pesquisar Diretório")
+print ("2 - Pesquisar Subdomínios")
+resultado=input("Quer executar qual pesquisa? ")
+# Voltar para o Main
+if resultado=="0":
+  if __name__ == '__main__':
+    cmd = r"python main.py"
+    subprocess.call(cmd, shell=True)
+# Diretório
+elif resultado=="1":
+  for i in lista:
+    url_check = "https://" + url + i
+    r = requests.get (url_check)
+    if r.status_code == 200:
+      print (url_check, "-", r.status_code)
+      print ("")
+    else:
+      continue
+# Subdomínios
+elif resultado=="2":
+  for i in lista:
+    sub_check = f"https://{i}.{url}"
+    try:
+      r = requests.get (sub_check)
+      print (sub_check, "-", r.status_code)
+      print ("")
+    except:
+      print ("Except em"+i)
+      continue
+# Retorna para o Exercício 3
+else:
+  print ("Retorna")
+  if __name__ == '__main__':
+    cmd = r"python exercicio3.py"
+    subprocess.call(cmd, shell=True)
 if __name__ == '__main__':
-  cmd = r"python main.py"
+  cmd = r"python exercicio3.py"
   subprocess.call(cmd, shell=True)
--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import subprocess
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
 
-resultado=input("Quer executar qual exercicio? ")
-if resultado=="1":
+# Menu
+print ("0 - Fechar")
+print ("1 - Exercício")
+print ("2 - Exercício")
+print ("3 - Exercício")
+resultado=input("Quer executar qual exercício? ")
+# Fecha o programa
+if resultado=="0":
+  print ("Fechando programa!")
+# Abre o Exercício 1
+elif resultado=="1":
   if __name__ == '__main__':
     cmd = r"python exercicio1.py"
     subprocess.call(cmd, shell=True)
+# Abre o Exercício 2
 elif resultado=="2":
   if __name__ == '__main__':
     cmd = r"python exercicio2.py"
     subprocess.call(cmd, shell=True)
+# Abre o Exercício 3
 elif resultado=="3":
   if __name__ == '__main__':
     cmd = r"python exercicio3.py"
     subprocess.call(cmd, shell=True)
-elif resultado=="4":
-  if __name__ == '__main__':
-    cmd = r"python exercicio4.py"
-    subprocess.call(cmd, shell=True)
+# Retorna ao Main
 else:
-    print ("Fechar")
+    print ("Retorna")
+    if __name__ == '__main__':
+      cmd = r"python main.py"
+      subprocess.call(cmd, shell=True)
